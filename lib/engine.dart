2a91383bd7bfb48a3051124b2ca02868fccdc497
Ahmed_hamdy <118023886+SiNfuuL@users.noreply.github.com>
2025-02-01 02:34:57 +0200

    refactor:  improve code readability in engine and view model

--- lib/engine.dart
+++ lib/engine.dart
@@ -1,17 +1,11 @@
-// lib/calculator_engine.dart
 
 import 'package:math_expressions/math_expressions.dart';
 
-/// A class that evaluates arithmetic expressions with advanced support,
-/// including trigonometric functions, exponentiation, parentheses, and percentage.
 class CalculatorEngine {
-  /// Evaluates the provided [expression] string and returns the computed value.
   double evaluateExpression(String expression) {
     try {
-      // Replace "%" with "/100" for percentage evaluation.
       final String expStr = expression.replaceAll('%', '/100');
 
-      // Parse and evaluate using math_expressions.
       final Parser parser = Parser();
       final Expression exp = parser.parse(expStr);
       final ContextModel cm = ContextModel();
